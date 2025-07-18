ca019b0fb260ae5a53c1625194dfc916c2694790
chintoz <jacintomena@gmail.com>
2025-07-19 00:48:12 +0200

    Changes to create BeerController OpenAPI documentation

--- src/main/java/es/menasoft/juniemvc/models/BeerDto.java
+++ src/main/java/es/menasoft/juniemvc/models/BeerDto.java
@@ -8,15 +8,18 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record BeerDto(
+        // read-only
         Integer id,
         Integer version,
-        
+
         @NotBlank(message = "Beer name is required")
         String beerName,
-        
+
+        // style of the beer, ALE, PALE ALE, IPA, etc.
         @NotBlank(message = "Beer style is required")
         String beerStyle,
-        
+
+        // Universal Product Code, a 13-digit number assigned to each unique beer product by the Federal Bar Assocation
         @NotBlank(message = "UPC is required")
         String upc,
         
@@ -27,7 +30,10 @@ public record BeerDto(
         @NotNull(message = "Price is required")
         @Positive(message = "Price must be positive")
         BigDecimal price,
-        
+
+        // read-only create data
         LocalDateTime createdDate,
+
+        // read-only update date
         LocalDateTime updateDate
 ) {}
